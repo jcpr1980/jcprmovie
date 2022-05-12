44f8d2a38a15add9830656d97d1a90192ade3450
jcpr1980 <83119867+jcpr1980@users.noreply.github.com>
2022-05-12 07:08:11 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/devsuperior/jcprmovie/JcprmovieApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.jcprmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcprmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcprmovieApplication.class, args);
+	}
+
+}
